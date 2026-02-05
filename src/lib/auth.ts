b3afc1c2d76b3c0f303ce72ced05a7c939e7fdaa
0xdolores <rubairakib11@gmail.com>
2026-02-05 22:16:36 +0600

    feat: Add better-auth and refactor

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,22 +1,11 @@
 import { betterAuth } from "better-auth";
-import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
-
-// Create the connection pool
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-// Create the adapter
-const adapter = new PrismaPg(pool);
-
-// Create Prisma client with the adapter
-const prisma = new PrismaClient({ adapter });
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "../db";
 
 export const auth = betterAuth({
-  database: prismaAdapter(prisma, {
-    provider: "postgresql", 
+  baseURL: process.env.BETTER_AUTH_BASE_URL || "http://localhost:3000",
+  database: drizzleAdapter(db, {
+    provider: "pg",
   }),
+  // ... other config
 });
